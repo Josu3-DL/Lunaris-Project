5ec7fe11987b3b920d2f912e3193f1aac92eb658
Cris272500 <cristopherquincer@gmail.com>
2024-10-25 08:46:10 -0600

    time, key signature y clef agregados
    Se agregan estos campos al modelo, y se pueden visualizar sus valores formateados

--- Core/models.py
+++ Core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from music21 import converter, note, interval
+from music21 import converter, note, interval, chord
 
 # Create your models here.
 class Partitura(models.Model):
@@ -10,15 +10,39 @@ class Partitura(models.Model):
     notas_transpuestas = models.JSONField(null=True, blank=True)
     notas_normales = models.JSONField(null=True, blank=True)
 
-    # funcion que se encarga de obtener tanto las notas transpuestas como las notas normales
-    def get_notes(self, intervalo_transposicion='M2'):
-        # CARGAR EL ARCHIVO MUSICXML
-        score = converter.parse(self.archivo.path)
-        
-        intervalo = interval.Interval(intervalo_transposicion)
-        transpored_score = score.transpose(intervalo)
+    # nuevos campos
+    time_signature = models.CharField(max_length=10, null=True, blank=True)
+    key_signature = models.CharField(max_length=20, null=True, blank=True)  # se aumenta el max_length para mostrar mayor/menor
+    clef = models.CharField(max_length=10, null=True, blank=True)
 
-        # aqui extraemos las notas normales
+    # función que se encarga de obtener tanto las notas transpuestas como las notas normales
+    def get_notes(self, intervalo_transposicion='M2'):
+        # Cargar el archivo MusicXML
+        score = converter.parse(self.archivo.path)
+
+        # Obtener la firma de tiempo (time signature)
+        time_signatures = score.parts[0].getTimeSignatures()
+        if time_signatures:
+            time_signature = time_signatures[0]
+            # Guardar como numerador/denominador
+            self.time_signature = f"{time_signature.numerator}/{time_signature.denominator}"
+
+        # Obtener la firma tonal (key signature)
+        key_signature = score.analyze('key')
+        if key_signature:
+            # Guardar la tonalidad con el nombre musical (mayor/menor)
+            self.key_signature = f"{key_signature.tonic.name} {key_signature.mode}"
+
+        # Obtener la clave (clef)
+        clefs = score.recurse().getElementsByClass('Clef')
+        if clefs:
+            self.clef = clefs[0].name  # Mostrar solo el nombre de la clave
+
+        # Proceso de transposición
+        intervalo = interval.Interval(intervalo_transposicion)
+        transposed_score = score.transpose(intervalo)
+
+        # Extraer las notas normales (incluyendo acordes)
         notas_normales = []
         for element in score.recurse():
             if isinstance(element, note.Note):
@@ -26,33 +50,46 @@ class Partitura(models.Model):
                     'nota': element.nameWithOctave,
                     'duracion': element.quarterLength
                 })
-        
-        # aqui extraemos las notas transpuestas
-        notas_transpuestas = []
+            elif isinstance(element, chord.Chord):
+                notas_normales.append({
+                    'nota': [n.nameWithOctave for n in element.notes],
+                    'duracion': element.quarterLength
+                })
 
-        for element in transpored_score.recurse():
+        # Extraer las notas transpuestas (incluyendo acordes)
+        notas_transpuestas = []
+        for element in transposed_score.recurse():
             if isinstance(element, note.Note):
                 notas_transpuestas.append({
                     'nota': element.nameWithOctave,
                     'duracion': element.quarterLength
                 })
+            elif isinstance(element, chord.Chord):
+                notas_transpuestas.append({
+                    'nota': [n.nameWithOctave for n in element.notes],
+                    'duracion': element.quarterLength
+                })
 
-        # guardamos las notas en los campos JSON
+        # Guardar las notas y la metadata en los campos JSON
         self.notas_transpuestas = notas_transpuestas
         self.notas_normales = notas_normales
         self.save()
 
-        # aqui analizamos la tonalidad de la partitura
+        # Aquí se analiza la tonalidad de la partitura
         key = score.analyze('key')
         print(f"La llave: {key}")
 
-    # definiendo serializador para los json de notas transpuestas y notas normales
     def serialize(self):
         return {
-            'notas_transupuestas': self.notas_transupuestas,
-            'notas_normales': self.notas_normales
+            'titulo': self.titulo,
+            'autor': self.autor,
+            'descripcion': self.descripcion,
+            'time_signature': self.time_signature,
+            'key_signature': self.key_signature,
+            'clef': self.clef,
+            'notas_transpuestas': self.notas_transpuestas,
+            'notas_normales': self.notas_normales,
         }
-    
 
     def __str__(self):
-        return self.archivo.name
+        return self.titulo
--- pruebas/app.py
+++ pruebas/app.py
@@ -1,29 +1,54 @@
-from music21 import converter, note
+from music21 import converter, note, interval
 
 # Cargar el archivo MusicXML
 score = converter.parse('playthatsheet-Paspie.musicxml')
 
-interval = 'M2'  # Intervalo de transposición, por ejemplo, una segunda mayor
-transposed_score = score.transpose(interval)
+# Obtener la clave (clef), la firma de tiempo y la firma tonal
+clefs = score.recurse().getElementsByClass('Clef')
+time_signatures = score.parts[0].getTimeSignatures()
+key_signature = score.analyze('key')
 
-# Extraer las notas de todas las páginas
+# Imprimir las claves
+if clefs:
+    clef_name = clefs[0].name
+    print(f'Clave: {clef_name}')
+else:
+    print('No se encontraron claves')
+
+# Imprimir la firma de tiempo de manera más amigable
+if time_signatures:
+    time_signature = time_signatures[0]
+    print(f'Firma de tiempo: {time_signature.numerator}/{time_signature.denominator}')
+else:
+    print('No se encontró firma de tiempo')
+
+# Imprimir la firma tonal (key signature)
+print(f'Firma tonal: {key_signature}')
+
+# Intervalo de transposición
+intervalo = interval.Interval('M2')
+transposed_score = score.transpose(intervalo)
+
+# Extraer las notas originales
 notes = []
 for element in score.recurse():
     if isinstance(element, note.Note):
         notes.append(element)
 
+# Extraer las notas transpuestas
 transposed_notes = []
 for element in transposed_score.recurse():
     if isinstance(element, note.Note):
         transposed_notes.append(element)
 
-# Imprimir las notas
+# Imprimir las notas originales
 for n in notes:
     print(f'Nota: {n.nameWithOctave}, Duración: {n.quarterLength}')
 
-
+# Imprimir las notas transpuestas
 for n in transposed_notes:
     print(f'Nota transpuesta: {n.nameWithOctave}, Duración: {n.quarterLength}')
 
+# Analizar la tonalidad
 key = score.analyze('key')
-print(key)
+print(f'Llave analizada: {key}')
