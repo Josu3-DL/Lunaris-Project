29053c0b80d866ad877cc2f898f51167e69e6b81
ChofiBv <israelruiztorres6@gmail.com>
2024-10-29 23:38:10 -0600

    Subiendo plantilla 💩

---
+++ Frontend/main.js
@@ -0,0 +1,7 @@
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import { home } from './src/index.js'
+import "./style.css"
+
+home()
