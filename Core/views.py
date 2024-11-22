f511fbf50be829c0215b07da51c06b0f74b5b055
Puntitoa <josueduarte2131@gmail.com>
2024-11-22 01:04:31 -0600

    Se implemento la conversion de archivo a audio a la api partiture

--- Core/views.py
+++ Core/views.py
@@ -13,6 +13,9 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import action
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404,HttpResponse,render
+from music21 import converter, tempo
+from midi2audio import FluidSynth
+
 
 class LoginView(APIView):
     def post(self, request, *args, **kwargs):
@@ -65,6 +68,66 @@ class PartitureViewsets(viewsets.ModelViewSet):
         partitura = serializer.save(user = user)
         partitura.get_notes()  # Ejecuta el procesamiento de notas después de actualizar la partitura
 
+  
+    @action(detail=True, methods=['post'], url_path='convertir_audio')
+    def convertir_audio(self, request, pk=None):
+        try:
+            # Obtener la partitura desde el objeto
+            partitura = self.get_object()
+
+            # Verificar si la partitura tiene un archivo asociado
+            if not partitura.archivo:
+                return Response({'error': 'No se encontró un archivo asociado a la partitura'}, status=status.HTTP_400_BAD_REQUEST)
+
+            input_file = partitura.archivo.path  # Asegúrate de que 'archivo' es un campo FileField
+
+            # Parsear el archivo MusicXML
+            try:
+                score = converter.parse(input_file)
+            except Exception as e:
+                return Response({'error': f'Error al procesar el archivo MusicXML: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Establecer un tempo por defecto
+            default_tempo = tempo.MetronomeMark(number=90)
+            score.insert(0, default_tempo)
+
+            # Guardar el archivo MIDI
+            midi_file = 'output.mid'
+            score.write('midi', fp=midi_file)
+
+            # Verificar si el archivo MIDI se generó correctamente
+            if not os.path.exists(midi_file):
+                return Response({'error': 'El archivo MIDI no fue generado correctamente.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            
+            # Convertir MIDI a WAV usando FluidSynth
+            soundfont = 'Core\static\Core\soundfont\MS_Basic.sf3'  # Ruta al archivo SoundFont
+            if not os.path.exists(soundfont):
+                return Response({'error': 'El archivo SoundFont no se encuentra en la ruta especificada.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Obtener el nombre del archivo original de la partitura
+            '''
+            archivo_nombre = os.path.basename(input_file)
+            nombre_sin_extension, _ = os.path.splitext(archivo_nombre)
+            '''
+
+            audio_file_wav = f'{partitura.titulo}.wav'
+            fs = FluidSynth(soundfont)
+            fs.midi_to_audio(midi_file, audio_file_wav)
+
+            # Verificar si el archivo de audio se generó correctamente
+            if not os.path.exists(audio_file_wav):
+                return Response({'error': 'El archivo de audio WAV no fue generado correctamente.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Leer el archivo de audio y enviarlo como respuesta
+            with open(audio_file_wav, 'rb') as audio_file:
+                response = HttpResponse(audio_file, content_type='audio/wav')
+                response['Content-Disposition'] = f'attachment; filename="{partitura.titulo}.wav"'
+                return response
+
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
     @action(detail=True, methods=['post'],url_path='generar_notas')
     def generar_notas(self, request, pk=None):
         try:
